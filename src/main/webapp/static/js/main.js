978c04b868f9bb12b926684b3f61d7d39977d87b
Harnet69 <harnet69@gmail.com>
2020-03-24 18:42:34 +0100

    Add JavaScript to count products in a cart

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -9,3 +9,15 @@ function mySubmit(theForm) {
         }
     });
 }
+
+function addListenerToButton(){
+    submitBtns = document.getElementsByClassName("submit");
+    productCounterLabel = document.getElementsByClassName("quantity")[0];
+    for(i=0; i<submitBtns.length; i++){
+        submitBtns[i].addEventListener("click", function () {
+            console.log("push!");
+            productCounterLabel.textContent = parseInt(productCounterLabel.textContent)+1;
+        })
+    }
+}
+addListenerToButton();
