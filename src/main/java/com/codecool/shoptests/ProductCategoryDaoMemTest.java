0520f8143780ebbee964673fdb4d21fc036f0116
DominikKublik <dominikkrzysztofkublik@gmail.com>
2020-03-25 10:47:09 +0100

    Fixed ProductCategoryDaoMemTest so it works with Singleton class

--- src/main/java/com/codecool/shoptests/ProductCategoryDaoMemTest.java
+++ src/main/java/com/codecool/shoptests/ProductCategoryDaoMemTest.java
@@ -3,33 +3,45 @@ package com.codecool.shoptests;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.model.ProductCategory;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProductCategoryDaoMemTest {
 
-    ProductCategoryDao productCategoryDao;
-    ProductCategory testCategory = new ProductCategory("TestName", "TestDepartment", "TestDescription");
+    private ProductCategoryDao productCategoryDao = ProductCategoryDaoMem.getInstance();
+    private ProductCategory testCategory = new ProductCategory("TestName", "TestDepartment", "TestDescription");
+    private ProductCategory testCategoryTwo = new ProductCategory("TestName2", "TestDepartment2", "TestDescription2");
+    private String expected = "id: 1," +
+            "name: TestName, " +
+            "department: TestDepartment, " +
+            "description: TestDescription";
 
-    @BeforeEach
-    void setUp() {
-        productCategoryDao = ProductCategoryDaoMem.getInstance();
-    }
+    private String expectedTwo = "id: 2," +
+            "name: TestName2, " +
+            "department: TestDepartment2, " +
+            "description: TestDescription2";
+
 
     @Test
-    void testAddOne() {
+    void testAdd() {
         productCategoryDao.add(testCategory);
-        String expected = "id: 1," +
-                "name: TestName, " +
-                "department: TestDepartment, " +
-                "description: TestDescription";
+        productCategoryDao.add(testCategoryTwo);
+
         assertEquals(expected, productCategoryDao.getAll().get(0).toString());
+        assertEquals(expectedTwo, productCategoryDao.getAll().get(1).toString());
     }
 
-    void multipleAddTests() {
 
+    @Test
+    void find() {
+        assertEquals(expectedTwo, productCategoryDao.find(2).toString());
     }
 
+
+    @Test
+    void remove() {
+        productCategoryDao.remove(1);
+        assertEquals(expectedTwo, productCategoryDao.getAll().get(0).toString());
+    }
 }
